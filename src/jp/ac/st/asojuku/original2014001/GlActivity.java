7241173eb6b6f442c4725382df3cafd6b8b92aae
student <student@PC111S05.fgakusei.local>
2014-05-29 14:58:35 +0900

    おみくじアプリ

---
+++ src/jp/ac/st/asojuku/original2014001/GlActivity.java
@@ -0,0 +1,15 @@
+package jp.ac.st.asojuku.original2014001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gl_activity);
+	}
+
+}
---
+++ src/jp/ac/st/asojuku/original2014001/MainActivity.java
@@ -0,0 +1,78 @@
+package jp.ac.st.asojuku.original2014001;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.EditText;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+			case R.id.btnOK:
+				//EditText etv = (EditText)findViewById(R.id.edtName);
+				//String inputVgl = etv.getText().toString();
+
+				Random rnd = new Random();
+
+				int ran = rnd.nextInt(4);
+
+				Intent intent = null;
+
+				switch(ran) {
+					case 0:
+
+						intent = new Intent(MainActivity.this,VglActivity.class);
+						startActivity(intent);
+						break;
+					case 1:
+
+						intent = new Intent(MainActivity.this,GlActivity.class);
+						startActivity(intent);
+						break;
+					case 2:
+
+						intent = new Intent(MainActivity.this,LuckActivity.class);
+						startActivity(intent);
+						break;
+					case 3:
+
+						intent = new Intent(MainActivity.this,BlActivity.class);
+						startActivity(intent);
+						break;
+				}
+
+		}
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
